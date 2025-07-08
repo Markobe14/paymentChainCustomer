afa2c511eac5397a325766960aed8b4543c95aae
VI5G01T <VI5G01T@VI5G01T08ALMXCO.telcel.net>
2025-07-07 20:36:57 -0500

    MA - Creacion entidad customer y repository

---
+++ customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.paymentchain.customer.repository;
+
+import com.paymentchain.customer.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    
+    
+    
+}
